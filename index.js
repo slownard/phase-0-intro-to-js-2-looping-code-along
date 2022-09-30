b27fe4cb508590ebb2cb77570a047ae469405eb3
Bernard Legaspi <blegaspi26@gmail.com>
2022-09-30 10:47:31 -0400

    commit tree 9b135d3b924b2cc7f08a681bef46b010c5763047

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solutions in this file
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+
+function wrapGift(gift) {
+    console.log(`Wrapped ${gift} and added a bow!`);
+}
+
+wrapGift(gifts[0]);
